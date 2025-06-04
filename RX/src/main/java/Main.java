43a0ce736dc5d5043a7fa66e13dce1d8701ec982
wrth1337 <benjamin.wirth98@gmail.com>
2025-06-04 14:59:34 +0200

    Start interpreting user-code in the main

--- RX/src/main/java/Main.java
+++ RX/src/main/java/Main.java
@@ -1,44 +1,62 @@
 import ast.*;
 import engine.RewriteEngine;
 import eval.Evaluator;
-import kernel.KernelRules;
 import lexer.Lexer;
-import lexer.Token;
-import lexer.TokenType;
+import parser.Parser;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Main {
-    public static void main(String[] args) {
-//        RewriteEngine engine = new RewriteEngine(KernelRules.defaultRules());
-//        Evaluator evaluator = new Evaluator(engine);
-//
-//        Expr expr = new App("add", List.of(new IntLiteral(2), new IntLiteral(3)));
-//        Expr result = evaluator.evaluate(expr);
-//        System.out.println("Original:" + expr);
-//        System.out.println("Result:" + result + "\n");
-//
-//        Expr expr2 = new App("add", List.of(new App("add", List.of(new IntLiteral(2), new IntLiteral(3))), new IntLiteral(10)));
-//        Expr result2 = evaluator.evaluate(expr2);
-//        System.out.println("Original:" + expr2);
-//        System.out.println("Result:" + result2 + "\n");
-//
-//        Expr expr3 = new App("eq", List.of(new IntLiteral(10), new App("mul", List.of(new IntLiteral(2), new IntLiteral(3)))));
-//        Expr result3 = evaluator.evaluate(expr3);
-//        System.out.println("Original:" + expr3);
-//        System.out.println("Result:" + result3 + "\n");
-//
-//        Expr expr4 = new App("eq", List.of(new IntLiteral(10), new App("div", List.of(new IntLiteral(1000), new IntLiteral(100)))));
-//        Expr result4 = evaluator.evaluate(expr4);
-//        System.out.println("Original:" + expr4);
-//        System.out.println("Result:" + result4 + "\n");
+    public static void main(String[] args) throws IOException {
+        //TODO: code should be read from a file -> args...
+        //TODO: Add more args/flags such as verbose mode, debug mode, repl(?) etc...
+        String code = """
+        fact(12)
+        """;
 
-        String code = "def double(x) = x + x";
-        Lexer lexer = new Lexer(code);
-        while(true){
-            Token token = lexer.nextToken();
-            System.out.println(token);
-            if(token.type().equals(TokenType.EOF)) break;
+
+        String preludeCode = Files.readString(Path.of("src/main/resources/rx_prelude.rx"));
+        String fullCode = preludeCode + "\n" + code;
+
+
+
+        Parser parser = new Parser(new Lexer(fullCode));
+        List<TopLevelItem> items = parser.parse();
+
+
+
+        List<Rule> rules = new ArrayList<>();
+        List<Expr> expressions = new ArrayList<>();
+        for (TopLevelItem item : items) {
+            if (item instanceof Rule rule) {
+                rules.add(rule);
+            } else if (item instanceof Expr expr) {
+                expressions.add(expr);
+            }
         }
+
+        RewriteEngine engine = new RewriteEngine(rules);
+        Evaluator evaluator = new Evaluator(engine);
+
+        Path outputDir = Path.of("out");
+        Files.createDirectories(outputDir);
+
+        //TODO: Output should be written to a single file, not multiple files...
+        //Easier for debugging for now... maybe an extra flag later?
+        for (int i = 0; i < expressions.size(); i++) {
+            Expr original = expressions.get(i);
+            Expr result = evaluator.evaluate(original);
+
+            System.out.println("Expression " + (i + 1) + ":\n" + result.toString());
+
+            Path outFile = outputDir.resolve("result_" + (i + 1) + ".rx");
+            Files.writeString(outFile, result.toString());
+        }
+
+        System.out.println("Program successfully rewritten");
     }
 }
