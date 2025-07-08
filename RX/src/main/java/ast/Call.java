54bd98d828fc52af24674a09693e78add57eba9f
wrth1337 <mail@hellowirth.de>
2025-07-08 14:37:40 +0200

    Add namespace to Call-Record.

--- RX/src/main/java/ast/Call.java
+++ RX/src/main/java/ast/Call.java
@@ -2,9 +2,21 @@ package ast;
 
 import java.util.List;
 
-public record Call(String function, List<Expr> arguments) implements Expr {
+public record Call(String namespace, String function, List<Expr> arguments) implements Expr {
     @Override
     public String toString(){
-        return function + arguments.toString();
+        return (namespace != null ? namespace + "." : "") + function + getArgumentsString();
+    }
+
+    private String getArgumentsString(){
+        StringBuilder arguments = new StringBuilder();
+        arguments.append("(");
+        for(Expr arg : this.arguments){
+            arguments.append(arg.toString());
+            arguments.append(", ");
+        }
+        if(arguments.length() > 1) arguments.delete(arguments.length() - 2, arguments.length());
+        arguments.append(")");
+        return arguments.toString();
     }
 }
