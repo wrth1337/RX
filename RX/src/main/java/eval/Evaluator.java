8f0a3a167d628565f93273767e1a647979e54875
wrth1337 <mail@hellowirth.de>
2025-07-11 16:23:51 +0200

    Refactor evaluateWithTrace in evaluator.

--- RX/src/main/java/eval/Evaluator.java
+++ RX/src/main/java/eval/Evaluator.java
@@ -2,8 +2,10 @@ package eval;
 
 import ast.*;
 import engine.RewriteEngine;
+import engine.RewriteResult;
 
 import java.util.List;
+import java.util.Optional;
 
 public class Evaluator {
     private final RewriteEngine engine;
@@ -47,46 +49,46 @@ public class Evaluator {
         return expr;
     }
 
-//    public Expr evaluateWithTrace(Expr expr, List<String> trace) {
-//        if (expr instanceof BinaryOp bin) {
-//            String fname = switch (bin.op()) {
-//                case ADD -> "add";
-//                case SUB -> "sub";
-//                case MUL -> "mul";
-//                case DIV -> "div";
-//                case MOD -> "mod";
-//                case EQ  -> "eq";
-//                case LT  -> "lt";
-//                case NQ -> "nq";
-//                case GT  -> "gt";
-//                case LE  -> "le";
-//                case GE  -> "ge";
-//            };
-//            Expr left = evaluateWithTrace(bin.left(), trace);
-//            Expr right = evaluateWithTrace(bin.right(), trace);
-//            return evaluateWithTrace(new Call("tbd",fname, List.of(left, right)), trace);
-//        }
-//
-//        if (expr instanceof Call call) {
-//            List<Expr> reducedArgs = call.arguments().stream()
-//                    .map(arg -> evaluateWithTrace(arg, trace))
-//                    .toList();
-//            Call reducedCall = new Call("tbd",call.function(), reducedArgs);
-//            Optional<RewriteResult> rewritten = engine.rewriteWithRule(reducedCall);
-//
-//
-//            if (rewritten.isPresent() && !rewritten.get().result().equals(expr)) {
-//                RewriteResult rr = rewritten.get();
-//                trace.add("[%d] Expression: %s\n     Rule: %s\n     Result: %s".formatted(
-//                        trace.size() + 1,
-//                        reducedCall,
-//                        rr.rule(),
-//                        rr.result()
-//                ));
-//                return evaluateWithTrace(rewritten.get().result(), trace);
-//            }
-//        }
-//        return expr;
-//    }
+    public Expr evaluateWithTrace(Expr expr, List<String> trace, String context) {
+        if (expr instanceof BinaryOp bin) {
+            String fname = switch (bin.op()) {
+                case ADD -> "add";
+                case SUB -> "sub";
+                case MUL -> "mul";
+                case DIV -> "div";
+                case MOD -> "mod";
+                case EQ  -> "eq";
+                case LT  -> "lt";
+                case NQ -> "nq";
+                case GT  -> "gt";
+                case LE  -> "le";
+                case GE  -> "ge";
+            };
+            Expr left = evaluateWithTrace(bin.left(), trace, context);
+            Expr right = evaluateWithTrace(bin.right(), trace, context);
+            return evaluateWithTrace(new Call(null,fname, List.of(left, right)), trace, context);
+        }
+
+        if (expr instanceof Call call) {
+            List<Expr> reducedArgs = call.arguments().stream()
+                    .map(arg -> evaluateWithTrace(arg, trace,  context))
+                    .toList();
+            Call reducedCall = new Call(call.namespace(),call.function(), reducedArgs);
+            String namespace = call.namespace() == null ? context : call.namespace();
+            Optional<RewriteResult> rewritten = engine.rewriteWithRule(reducedCall, context);
+            if (rewritten.isPresent() && !rewritten.get().result().equals(expr)) {
+                RewriteResult rr = rewritten.get();
+                trace.add("[%d] Expression: %s\n     Context: %s\n     Rule: %s\n     Result: %s".formatted(
+                        trace.size() + 1,
+                        reducedCall,
+                        namespace,
+                        rr.rule(),
+                        rr.result()
+                ));
+                return evaluateWithTrace(rewritten.get().result(), trace, namespace);
+            }
+        }
+        return expr;
+    }
 
 }
